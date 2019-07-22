f022dd82bd9259b0fb6c558eb763fcf6535f87c9
rudolif <rudolifrudolif@gmail.com>
2019-07-22 16:10:30 +0200

    Vlad commit

---
+++ app/js/common.js
@@ -0,0 +1,216 @@
+$(function() {
+
+//-------------------------------попандер---------------------------------------
+  $('.modal').popup({transition: 'all 0.3s'});
+
+//------------------------------------form-------------------------------------------
+  $('input[type="tel"]').mask('+0 (000) 000-00-00');
+
+  jQuery.validator.addMethod("phoneno", function(phone_number, element) {
+     return this.optional(element) || phone_number.match(/\+[0-9]{1}\s\([0-9]{3}\)\s[0-9]{3}-[0-9]{2}-[0-9]{2}/);
+  }, "Введите Ваш телефон");
+
+  $(".form").each(function(index, el) {
+    $(el).addClass('form-' + index);
+
+    $('.form-' + index).validate({
+      rules: {
+        phone: {
+          required: true,
+          phoneno: true
+        },
+        name: 'required',
+      },
+      messages: {
+        name: "Введите Ваше имя",
+        phone: "Введите Ваш телефон",
+        mail: "Введите Ваш Email",
+      },
+      submitHandler: function(form) {
+        var t = {
+          name: jQuery('.form-' + index).find("input[name=name]").val(),
+          phone: jQuery('.form-' + index).find("input[name=phone]").val(),
+          mail: jQuery('.form-' + index).find("input[name=mail]").val(),
+          subject: jQuery('.form-' + index).find("input[name=subject]").val()
+        };
+        ajaxSend('.form-' + index, t);
+      }
+    });
+
+  });
+
+  function ajaxSend(formName, data) {
+    jQuery.ajax({
+      type: "POST",
+      url: "sendmail.php",
+      data: data,
+      success: function() {
+        $(".modal").popup("hide");
+        $("#thanks").popup("show");
+        setTimeout(function() {
+          $(formName).trigger('reset');
+        }, 2000);
+      }
+    });
+  }
+
+//-------------------------скорость якоря---------------------------------------
+
+  $(".click").on("click","a", function (event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top - 60}, 950);
+  });
+
+  $('.tabs__wrap').hide();
+  $('.tabs__wrap:first').show();
+  $('.tabs ul a:first').addClass('active');
+   $('.tabs ul a').click(function(event){
+    event.preventDefault();
+    $('.tabs ul a').removeClass('active');
+    $(this).addClass('active');
+    $('.tabs__wrap').hide();
+     var selectTab = $(this).attr('href');
+    $(selectTab).fadeIn();
+  });
+
+  var swiper = new Swiper('.works__slider', {
+    freeMode: true,
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 50,
+    // init: false,
+    pagination: {
+      el: '.works__prev',
+      clickable: true,
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      }
+    }
+  });
+
+  var swiper = new Swiper('.reviews__slider', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 50,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+  });
+
+  var swiper = new Swiper('.kurs__slider', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 50,
+    // init: false,
+    pagination: {
+      el: '.kurs__prev',
+      clickable: true,
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      }
+    }
+  });
+
+  var swiper = new Swiper('.teacher__slider', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 50,
+    // init: false,
+    pagination: {
+      el: '.teacher__prev',
+      clickable: true,
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      }
+    }
+  });
+
+//------------------------------acardeon---------------------------
+
+  $(".inst__dropdown").slideUp("slow");
+  $(".inst__item").first('').addClass('active');
+  $(".active .inst__dropdown").slideDown("slow");
+
+  $(".inst__header").on("click", function(){
+    if ($(this).parent().hasClass('active')) {
+      $(this).parent().removeClass('active');
+      $(".inst__dropdown").slideUp("slow");
+    }
+    else {
+      $(".active .inst__dropdown").slideUp("slow");
+      $(".active").removeClass('active');
+      $(this).parent().addClass('active');
+      $(".active .inst__dropdown").slideDown("slow");
+    }
+  });
+
+});
+
+  $(".resultat__dropdown").slideUp("slow");
+  $(".resultat__item").first('').addClass('active');
+  $(".active .resultat__dropdown").slideDown("slow");
+
+  $(".resultat__header").on("click", function(){
+    if ($(this).parent().hasClass('active')) {
+      $(this).parent().removeClass('active');
+      $(".resultat__dropdown").slideUp("slow");
+    }
+    else {
+      $(".active .resultat__dropdown").slideUp("slow");
+      $(".active").removeClass('active');
+      $(this).parent().addClass('active');
+      $(".active .resultat__dropdown").slideDown("slow");
+    }
+  });
